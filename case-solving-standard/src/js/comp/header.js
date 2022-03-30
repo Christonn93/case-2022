53c3af4fe4849c933510a9eb7a63a1fc4e059764
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-03-30 14:20:30 +0200

    adding redame

--- case-solving-standard/src/js/comp/header.js
+++
@@ -1,5 +0,0 @@
-export function headerContent() {
-    const header = document.createElement("header");
-    header.classList.add("main_header");
-    header.innerHTML = `<img src="./" alt="company logo"/>`
-}
